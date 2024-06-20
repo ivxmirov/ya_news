b1457216ec0dfaf340a342c0c011fed482d0709c
ivxmirov <ivxmirov@gmail.com>
2024-06-20 22:41:48 +0400

    pytest test_routes are ready

---
+++ news/pytest_tests/test_routes.py
@@ -0,0 +1,53 @@
+import pytest
+from http import HTTPStatus
+
+from django.urls import reverse
+from pytest_django.asserts import assertRedirects
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('news:home', 'users:login', 'users:logout', 'users:signup')
+)
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_page_news_detail_availability_for_anonymous_user(client, news):
+    url = reverse('news:detail', args=(news.pk,))
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('news:edit', 'news:delete'),
+)
+def test_pages_availability_for_different_users(
+        parametrized_client, name, comment, expected_status
+):
+    url = reverse(name, args=(comment.pk,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+    'name', ('news:edit', 'news:delete')
+)
+def test_redirects(client, name, comment):
+    login_url = reverse('users:login')
+    url = reverse(name, args=(comment.pk,))
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
--- news/urls.py
+++ news/urls.py
@@ -6,11 +6,19 @@ app_name = 'news'
 
 urlpatterns = [
     path('', views.NewsList.as_view(), name='home'),
-    path('news/<int:pk>/', views.NewsDetailView.as_view(), name='detail'),
+    path(
+        'news/<int:pk>/',
+        views.NewsDetailView.as_view(),
+        name='detail'
+    ),
     path(
         'delete_comment/<int:pk>/',
         views.CommentDelete.as_view(),
         name='delete'
     ),
-    path('edit_comment/<int:pk>/', views.CommentUpdate.as_view(), name='edit'),
+    path(
+        'edit_comment/<int:pk>/',
+        views.CommentUpdate.as_view(),
+        name='edit'
+    )
 ]
